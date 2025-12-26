5af412abfb756749c8bb63323fbaf4282370be76
Iamnotagenius <kazakk9@gmail.com>
2025-12-27 01:11:36 +0300

    GIF support patch

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = 'e45faebc'
+version = '0.1.0+dev.e45faebc'
+linkified_version = '0.1.0+dev.[e45faebc](https://github.com/maunium/stickerpicker/commit/e45faebc74429cca3b064ea4b254ab3c820e7fa5)'
